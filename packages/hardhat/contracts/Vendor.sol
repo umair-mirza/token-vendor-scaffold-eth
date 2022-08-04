e079b6c828c6ca9290aa0add1ad88cf34a027540
Umair Mirza <umair_hp@hotmail.com>
2022-08-05 01:38:04 +0500

    completed the checkpoint-3

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,7 +6,9 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+
+  uint256 public constant tokensPerEth = 100;
 
   YourToken public yourToken;
 
@@ -16,7 +18,30 @@ contract Vendor is Ownable {
 
   // ToDo: create a payable buyTokens() function:
 
+  function buyTokens() public payable returns (uint256 tokenAmount) {
+    require(msg.value > 0, "Send ETH to buy some tokens");
+
+    uint256 amountToBuy = msg.value * tokensPerEth;
+
+    // check if the Vendor Contract has enough amount of tokens for the transaction
+    uint256 vendorBalance = yourToken.balanceOf(address(this));
+    require(vendorBalance >= amountToBuy, "Vendor contract has not enough tokens in its balance");
+
+    // Transfer token to the msg.sender
+    (bool sent) = yourToken.transfer(msg.sender, amountToBuy);
+    require(sent, "Failed to transfer token to user");
+
+    // emit the event
+    emit BuyTokens(msg.sender, msg.value, amountToBuy);
+
+    return amountToBuy;
+  }
+
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+
+  function withdraw() public onlyOwner {
+    payable(msg.sender).transfer(address(this).balance);
+  }
 
   // ToDo: create a sellTokens(uint256 _amount) function:
 
